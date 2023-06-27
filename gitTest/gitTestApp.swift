1b69ea204af5d5296ac1043dcfe5f99bf22305f2
Hahk3 <timelesstm@naver.com>
2023-06-27 10:52:39 +0900

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by USER on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
