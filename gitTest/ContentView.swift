3687945645d08ed875887a38e3635348fee3d04e
Hahk3 <timelesstm@naver.com>
2023-06-27 14:02:19 +0900

    3

--- gitTest/ContentView.swift
+++ gitTest/ContentView.swift
@@ -17,6 +17,8 @@ struct ContentView: View {
             Text("aaa")
             Text("bbb")
             Text("ccc")
+            Text("ccc")
+            Text("ccc")
         }
         .padding()
     }
